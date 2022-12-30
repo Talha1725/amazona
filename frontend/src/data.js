5116e29da32d36f22e89dd26c2b35c979f83c00c
Talha <talhanoor1001@gmail.com>
2022-12-30 21:28:02 +0500

    listing products from data file

---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+  products: [
+    {
+      name: 'Nike slim Shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg', //dimensions : 679 x 829
+      price: 120,
+      countInStock: '10',
+      brand: 'Nike',
+      rating: '4.5',
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Adidas Fit Shirt',
+      slug: 'adidas-fit-shirt',
+      category: 'Shirts',
+      image: '/images/p2.jpg', //dimensions : 679 x 829
+      price: 80,
+      countInStock: '10',
+      brand: 'Nike',
+      rating: '4.5',
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Nike slim pants',
+      slug: 'nike-slim-pants',
+      category: 'Pants',
+      image: '/images/p3.jpg',
+      price: 25,
+      countInStock: '15',
+      brand: 'Nike',
+      rating: '4.5',
+      numReviews: 14,
+      description: 'high quality pants',
+    },
+    {
+      name: 'Adidas slim Shirt',
+      slug: 'adidas-slim-shirt',
+      category: 'Pants',
+      image: '/images/p4.jpg',
+      price: 65,
+      countInStock: '5',
+      brand: 'Puma',
+      rating: '4.5',
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+  ],
+};
+
+export default data;
